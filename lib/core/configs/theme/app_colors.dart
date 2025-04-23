4bb683f6db0c028d21bafeb497acf6b566f4cc47
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-04-23 11:37:26 -0300

    theme: Agregamos los colores, vectores e imagenes

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff8E6CEF); // Violeta principal (ya tenés)
+  static const background = Color(0xff1D182A); // Fondo principal oscuro
+  static const secondBackground =
+      Color(0xff342F3F); // Fondo de tarjetas, botones oscuros
+
+  static const accent =
+      Color(0xffFFCE00); // Amarillo dorado para CTA / highlights
+  static const success = Color(0xff5EDB8C); // Verde suave para estados exitosos
+  static const error = Color(0xffEF6C6C); // Rojo pastel para errores
+  static const warning = Color(0xffFF9F45); // Naranja suave para alertas
+
+  static const textPrimary =
+      Color(0xffEDEDED); // Texto principal blanco-gris claro
+  static const textSecondary = Color(0xffB5B5B5); // Texto secundario gris medio
+
+  static const border = Color(0xff3D3747); // Bordes o líneas divisoras
+  static const card = Color(0xff2B2535); // Fondo de cards o inputs
+}
